a5978b9a0620ce46988cdf2c86b730b97abb0ed0
alexchun78 <alexchun78@gmail.com>
2022-10-31 15:38:28 +0900

    UI 매니저 작업하기 직전 commit
    Extension 기능까지 구현

--- MMODev/Assets/Scripts/UI/UIButton.cs
+++ MMODev/Assets/Scripts/UI/UIButton.cs
@@ -35,20 +35,20 @@ public class UIButton : UIBase
         Bind<GameObject>(typeof(GameObjects));
         Bind<Image>(typeof(Images));
 
-        Get<Text>((int)Texts.ScoreText).text = "Bind Text";
 
-        
+        // extention을 선언하여 사용하면, 아래처럼 코드를 나누지 않을 수 있다. 
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonCliecked);
 
         GameObject go = GetImage((int)Images.ItemIcon).gameObject;
         AddUIEvent(go, ((PointerEventData data) => { go.transform.position = data.position; }), Define.UIEvent.Drag);
     }
 
+    int score = 0;
+    public void OnButtonCliecked(PointerEventData data)
+    {
+        Debug.Log("Button Clicked");
+        score++;
 
-    //int score = 0;
-    //public void OnButtonCliecked()
-    //{
-    //    Debug.Log("Button Clicked");
-    //    score++;
-    //    _text.text = $"점수 : {score}";
-    //}
+        Get<Text>((int)Texts.ScoreText).text = $"점수 : {score}";
+    }
 }
