72f79bb48a90746c9bfba81ed0cc22b1486e9520
alexchun78 <alexchun78@gmail.com>
2022-11-08 19:09:27 +0900

    게임 서버 학습 9강 메모리 배리어
    하드웨어 단에서 최적화하는 기능이 수행되곤 한다.
    이 과정에서 코드의 진행 순서가 변경되기도 한다.
    이를 방지하기 위해 '메모리 배리어'라는 기능이 필요하다.

--- SampleServer/ServerCore/Program.cs
+++ SampleServer/ServerCore/Program.cs
@@ -4,38 +4,77 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // 메모리 배리어
+    // (A) 코드 재배치 방지
+    // (B) 가시성
+    // 1) Full Memory Barrior  (ASM MFENCE) : Store / Load 둘다 막는다.  -> Thread.MemoryBarrier();
+    // 2) Store Memory Barrior (ASM SFENCE) : Store만 막는다.
+    // 2) Load Memory Barrior (ASM LFENCE) : Load만 막는다.
     class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
 
+        static void Thread_1()
+        {
+            y = 1; // Store y
+
+            //-------------------------------
+            Thread.MemoryBarrier();
+
+            r1 = x; // Load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // Store x
+
+            //-------------------------------
+            Thread.MemoryBarrier();
+
+            r2 = y; // Load y
+        }
+
+        int _answer;
+        bool _complete;
+        void A()
+        {
+            _answer = 123;
+            Thread.MemoryBarrier(); // 기록 후
+            _complete = true;
+            Thread.MemoryBarrier();// 기록 후
+        }
+
+        void B()
+        {
+            Thread.MemoryBarrier();// 읽기 전
+            if (_complete)
+            {
+                Thread.MemoryBarrier(); // 읽기 전
+                Console.WriteLine(_answer);
+            }
+        }
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                var now = DateTime.Now.Ticks;
-                for(int c = 0; c < 10000; ++ c)
-                {
-                    for(int r = 0; r < 10000; ++r)
-                    {
-                        arr[c, r] = 1;
-                    }
-                }
-                var end = DateTime.Now.Ticks;
-                Console.WriteLine($"arr[c, r] 순서 걸린 시간 : {end - now}");
-            }
-            {
-                var now = DateTime.Now.Ticks;
-                for (int c = 0; c < 10000; ++c)
-                {
-                    for (int r = 0; r < 10000; ++r)
-                    {
-                        arr[r, c] = 1;
-                    }
-                }
-                var end = DateTime.Now.Ticks;
-                Console.WriteLine($"arr[r, c] 순서 걸린 시간 : {end - now}");
-            }
+                count++;
+                x = y = r1 = r2 = 0;
 
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
+            }
+            Console.WriteLine($"{count}번만에 탈출!");
         }
     }
 }
