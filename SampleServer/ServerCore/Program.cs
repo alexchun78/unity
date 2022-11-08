e68edf77ea38256689d5f82630654826acfb4dbc
alexchun78 <alexchun78@gmail.com>
2022-11-08 18:03:26 +0900

    게임 서버 학습 06강
    Thread간 공유하는 데이터 관련 내용 -> 공용 데이터 때문에 문제 발생 확률 높다. -> volatile 키워드 처음 배움 -> 사용 하지 않는 걸 추천함

---
+++ SampleServer/ServerCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("Thread Start");
+            while(_stop == false)
+            {
+                // wait for stop signal
+            }
+            Console.WriteLine("Thread End");
+        }
+
+        static void Main(string[] args)
+        {
+            Task task = new Task(ThreadMain);
+            task.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            task.Wait();
+            Console.WriteLine("종료 성공");
+        }
+    }
+}
